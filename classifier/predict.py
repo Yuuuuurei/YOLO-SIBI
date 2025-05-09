2a17507d63e7677e12801db883e10c0808c659be
Fahri Desrian <fahri.desrian@sci.ui.ac.id>
2025-05-09 10:25:04 +0700

    fixed predict script

--- classifier/predict.py
+++ classifier/predict.py
@@ -49,7 +49,6 @@ def load_model(model_name, num_classes, model_path):
 
 # ====== Transformasi Gambar ======
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
